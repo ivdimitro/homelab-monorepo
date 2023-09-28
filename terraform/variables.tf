65b32dafe54ace4f9e399d90f0f15636039e4a9a
Ivan Dimitrov <hipantcii@gmail.com>
2023-09-28 21:32:21 +0200

    add terraform test project

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,7 +6,7 @@
 variable "proxmox_api_url" {
     description = "This is the target Proxmox API endpoint"
     type = string
-    default = "https://192.168.2.31:8006/api2/json"
+    default = "https://10.10.0.7:8006/api2/json"
 }
 variable "proxmox_api_user" {
     description = "This is the Proxmox API user. Use root@pam or custom. Will need PVEDatastoreUser, PVEVMAdmin, PVETemplateUser permissions"
@@ -17,6 +17,7 @@ variable "proxmox_api_user" {
 variable "proxmox_api_pass" {
     description = "API user password. Required, sensitive, or use environment variable TF_VAR_proxmox_api_pass"
     sensitive = true
+    default = "R58t58FW!G^k37TB8Z$S@7Sh4dixkKguCs#Pr7YM&h4GYgbWbEATSd&EA%@3oSUo"
 }
 variable "proxmox_ignore_tls" {
     description = "Disable TLS verification while connecting"
